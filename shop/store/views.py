5123c39424546f2c5e63b26be573526127f25352
sardor <sarchik2000@gmail.com>
2024-04-30 18:14:32 +0500

    third

--- shop/store/views.py
+++ shop/store/views.py
@@ -4,7 +4,13 @@ from django.views.generic import ListView, DetailView
 
 class ProductList(ListView):
     model = Product
+    context_object_name = 'categories'
     extra_context = {
         'title': 'Store: Minishop'
     }
     template_name = 'store/product_list.html'
+
+    def get_queryset(self):
+        categories = Category.objects.filter(parent=None)
+        return categories
+    
